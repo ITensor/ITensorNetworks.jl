e501293f7a91de07c2b4f2a897473462dda4c6eb
Matthew Fishman <mfishman@flatironinstitute.org>
2021-12-16 15:28:38 -0500

    DataGraphs module

--- src/DataGraphs/examples/examples.jl
+++ src/DataGraphs/examples/examples.jl
@@ -1,19 +1,123 @@
 
-include(joinpath("..", "src", "SimpleMetaGraphs.jl"))
+include(joinpath("..", "src", "DataGraphs.jl"))
 
 g = grid((4,))
-vertex_metadata = dictionary([1 => "V1", 2 => "V2", 3 => "V3", 4 => "V4"])
-edge_metadata = dictionary([Edge(1, 2) => "E12"])
-mg = SimpleMetaGraph(g, vertex_metadata, edge_metadata)
+dg = DataGraph{String,Symbol}(g)
+@show !isassigned(dg, Edge(1, 2))
+@show !isassigned(dg, 1 => 2)
+@show !isassigned(dg, Edge(1 => 2))
+@show !isassigned(dg, 1 => 3)
+@show !isassigned(dg, 1)
+@show !isassigned(dg, 2)
+@show !isassigned(dg, 3)
+@show !isassigned(dg, 4)
 
-@show mg[1] == "V1"
-@show mg[2] == "V2"
-@show mg[3] == "V3"
-@show mg[4] == "V4"
+@show has_edge(dg, 1, 2)
+@show has_edge(dg, 1 => 2)
+@show !has_edge(dg, 1, 3)
+@show !has_edge(dg, 1 => 3)
+@show has_vertex(dg, 1)
+@show has_vertex(dg, 4)
+@show !has_vertex(dg, 0)
+@show !has_vertex(dg, 5)
 
-@show mg[1 => 2] == "E12"
-@show mg[1, 2] == "E12"
-@show mg[Edge(1, 2)] == "E12"
+dg[1] = "V1"
+dg[2] = "V2"
+dg[3] = "V3"
+dg[4] = "V4"
+@show isassigned(dg, 1)
+@show dg[1] == "V1"
+@show dg[2] == "V2"
+@show dg[3] == "V3"
+@show dg[4] == "V4"
 
-mg[1 => 4] = "E14"
-@show mg[1 => 4]
+dg[1 => 2] = :E12
+dg[2 => 3] = :E23
+dg[Edge(3, 4)] = :E34
+@show isassigned(dg, (1, 2))
+@show isassigned(dg, Edge(2, 3))
+@show isassigned(dg, 3 => 4)
+@show dg[Edge(1, 2)] == :E12
+@show dg[2 => 3] == :E23
+@show dg[3 => 4] == :E34
+
+vdata = dictionary([1 => "V1", 2 => "V2", 3 => "V3", 4 => "V4"])
+edata = dictionary([Edge(1, 2) => "E12", Edge(2, 3) => "E23", Edge(3, 4) => "E34"])
+dg = DataGraph(g, vdata, edata)
+
+@show dg[1] == "V1"
+@show dg[2] == "V2"
+@show dg[3] == "V3"
+@show dg[4] == "V4"
+
+@show dg[1 => 2] == "E12"
+@show dg[2 => 3] == "E23"
+@show dg[3 => 4] == "E34"
+
+@show DataGraph(g) isa DataGraph{Any,Any}
+@show DataGraph{String}(g) isa DataGraph{String,Any}
+@show DataGraph{<:Any,String}(g) isa DataGraph{Any,String}
+
+vdata = [1 => "V1", 2 => "V2", 3 => "V3", 4 => "V4"]
+edata = [(1, 2) => "E12", (2 => 3) => "E23", Edge(3, 4) => "E34"]
+dg = DataGraph(g, vdata, edata)
+
+@show dg[1] == "V1"
+@show dg[2] == "V2"
+@show dg[3] == "V3"
+@show dg[4] == "V4"
+
+@show dg[1 => 2] == "E12"
+@show dg[2 => 3] == "E23"
+@show dg[3 => 4] == "E34"
+
+vdata = ["V1", "V2", "V3", "V4"]
+edata = [(1, 2) => "E12", (2 => 3) => "E23", Edge(3, 4) => "E34"]
+dg = DataGraph(g, vdata, edata)
+
+@show dg[1] == "V1"
+@show dg[2] == "V2"
+@show dg[3] == "V3"
+@show dg[4] == "V4"
+
+@show dg[1 => 2] == "E12"
+@show dg[2 => 3] == "E23"
+@show dg[3 => 4] == "E34"
+
+vdata = ["V1", "V2", "V3", "V4"]
+edata = ["E12", "E23", "E34"]
+dg = DataGraph(g, vdata, edata)
+
+@show dg[1] == "V1"
+@show dg[2] == "V2"
+@show dg[3] == "V3"
+@show dg[4] == "V4"
+
+@show dg[1 => 2] == "E12"
+@show dg[2 => 3] == "E23"
+@show dg[3 => 4] == "E34"
+
+vdata = ["V1", "V2", "V3", "V4"]
+edata = [Edge(1, 2) => "E12", Edge(2, 3) => "E23"]
+dg = DataGraph(g; vertex_data=vdata, edge_data=edata)
+
+@show dg[1] == "V1"
+@show dg[2] == "V2"
+@show dg[3] == "V3"
+@show dg[4] == "V4"
+
+@show dg[1 => 2] == "E12"
+@show dg[2 => 3] == "E23"
+@show !isassigned(dg, 3 => 4)
+
+vdata = ["V1", "V2", "V3", "V4"]
+dg = DataGraph(g; vertex_data=vdata)
+
+@show dg[1] == "V1"
+@show dg[2] == "V2"
+@show dg[3] == "V3"
+@show dg[4] == "V4"
+
+@show !isassigned(dg, 1 => 2)
+@show !isassigned(dg, 2 => 3)
+@show !isassigned(dg, 3 => 4)
--- src/DataGraphs/src/DataGraphs.jl
+++ src/DataGraphs/src/DataGraphs.jl
@@ -1,10 +1,15 @@
-#module SimpleMetaGraphs
+module DataGraphs
+
+  import Base: get, getindex, setindex!, convert, show, isassigned, eltype
+
   using Dictionaries
+
+  # Dictionaries.jl patch
+  convert(::Type{Dictionary{I,T}}, dict::Dictionary{I,T}) where {I, T} = dict
+
   using Graphs
 
-  import Base: get, getindex, setindex!
-
-  import Graphs: edgetype, ne, nv
+  import Graphs: edgetype, ne, nv, vertices, edges, has_edge, has_vertex
 
   abstract type AbstractDataGraph{VD,ED,V,E} <: AbstractGraph{V} end
 
@@ -14,21 +19,151 @@
   edge_data(graph::AbstractDataGraph) = getfield(graph, :edge_data)
 
   # Graphs overloads
-  for f in [:edgetype, :nv, :ne]
+  for f in [:edgetype, :nv, :ne, :vertices, :edges, :eltype, :has_edge, :has_vertex]
     @eval begin
       $f(graph::AbstractDataGraph, args...) = $f(parent_graph(graph), args...)
     end
   end
 
-  getindex(graph::AbstractDataGraph{V}, v::V) where {V} = vertex_data(graph)[v]
-  getindex(graph::AbstractDataGraph, e::AbstractEdge) = edge_data(graph)[e]
-  getindex(graph::AbstractDataGraph, e::Pair) = graph[edgetype(graph)(e)]
-  getindex(graph::AbstractDataGraph{V}, src::V, dst::V) where {V} = graph[edgetype(graph)(src, dst)]
+  # Vertex or Edge trait
+  abstract type VertexOrEdge end
+  struct IsVertex <: VertexOrEdge end
+  struct IsEdge <: VertexOrEdge end
 
-  setindex!(graph::AbstractDataGraph{V}, x, v::V) where {V} = (vertex_data(graph)[v] = x; return graph)
-  setindex!(graph::AbstractDataGraph, x, e::AbstractEdge) = (edge_data(graph)[e] = x; return graph)
-  setindex!(graph::AbstractDataGraph, x, e::Pair) = (graph[edgetype(graph)(e)] = x; return graph)
-  setindex!(graph::AbstractDataGraph{V}, x, src::V, dst::V) where {V} = (graph[edgetype(graph)(src, dst)] = x; return graph)
+  is_vertex_or_edge(graph::AbstractGraph, args...) = error("$args don't represent a vertex or an edge for graph:\n$graph.")
+  is_vertex_or_edge(graph::AbstractGraph{V}, I::V) where {V} = IsVertex()
+  is_vertex_or_edge(graph::AbstractGraph, I::AbstractEdge) = IsEdge()
+  is_vertex_or_edge(graph::AbstractGraph{V}, I::Pair{V,V}) where {V} = IsEdge()
+  is_vertex_or_edge(graph::AbstractGraph{V}, I::Tuple{V,V}) where {V} = IsEdge()
+
+  data(::IsVertex, graph::AbstractDataGraph) = vertex_data(graph)
+  data(::IsEdge, graph::AbstractDataGraph) = edge_data(graph)
+  index_type(::IsVertex, graph::AbstractDataGraph, args...) = eltype(graph)(args...)
+  index_type(::IsEdge, graph::AbstractDataGraph, args...) = edgetype(graph)(args...)
+
+  for f in [:getindex, :isassigned]
+    @eval begin
+      $f(ve::VertexOrEdge, graph::AbstractDataGraph, args...) = $f(data(ve, graph), index_type(ve, graph, args...))
+      $f(graph::AbstractDataGraph, args...) = $f(is_vertex_or_edge(graph, args...), graph, args...)
+    end
+  end
+
+  setindex!(graph::AbstractDataGraph, x, args...) = setindex!(is_vertex_or_edge(graph, args...), graph, x, args...)
+  setindex!(ve::VertexOrEdge, graph::AbstractDataGraph, x, args...) = setindex!(data(ve, graph), x, index_type(ve, graph, args...))
+
+  #
+  # Helper functions for constructing AbstractDataGraph
+  #
+
+  function default_data(
+    index_type::Type,
+    data_type::Type,
+    indices_function::Function,
+    parent_graph::AbstractGraph,
+    ::Nothing
+  )
+    return similar(Indices(indices_function(parent_graph)), data_type)
+  end
+
+  # Custom dictionary-like constructor that only accepts Pair lists
+  function data_dict(
+    index_type::Type,
+    data_type::Type,
+    indices_function::Function,
+    parent_graph::AbstractGraph,
+    data::Vector{<:Pair}
+  )
+    indices = index_type.(first.(data))
+    values = convert(Vector{data_type}, last.(data))
+    return Dictionary{index_type,data_type}(indices, values)
+  end
+
+  # Custom dictionary-like constructor that only accepts Pair lists
+  function data_dict(
+    index_type::Type,
+    data_type::Type,
+    indices_function::Function,
+    parent_graph::AbstractGraph,
+    data::Vector
+  )
+    indices = indices_function(parent_graph)
+    values = convert(Vector{data_type}, data)
+    return Dictionary{index_type,data_type}(indices, values)
+  end
+
+  function default_data(
+    index_type::Type,
+    data_type::Type,
+    indices_function::Function,
+    parent_graph::AbstractGraph,
+    init_data
+  )
+    data = data_dict(
+      index_type,
+      data_type,
+      indices_function,
+      parent_graph,
+      init_data
+    )
+    return default_data(
+      index_type,
+      data_type,
+      indices_function,
+      parent_graph,
+      data
+    )
+  end
+
+  function default_data(
+    index_type::Type,
+    data_type::Type,
+    indices_function::Function,
+    parent_graph::AbstractGraph,
+    init_data::Dictionary
+  )
+    data = default_data(
+      index_type,
+      data_type,
+      indices_function,
+      parent_graph,
+      nothing
+    )
+    for i in eachindex(init_data)
+      if isassigned(init_data, i)
+        data[i] = init_data[i]
+      end
+    end
+    return data
+  end
+
+  data_type(::Nothing) = Any
+  data_type(::Vector{T}) where {T} = T
+  data_type(::Vector{Pair{S,T}}) where {S,T} = T
+
+  #
+  # Printing
+  #
+
+  function show(io::IO, mime::MIME"text/plain", graph::AbstractDataGraph)
+    println(io, "DataGraph with $(nv(graph)) vertices:")
+    show(io, mime, vertices(graph))
+    println(io, "\n")
+    println(io, "and $(ne(graph)) edge(s):")
+    for e in edges(graph)
+      show(io, mime, e)
+      println(io)
+    end
+    println(io)
+    println(io, "and vertex data:")
+    show(io, mime, vertex_data(graph))
+    println(io)
+    println(io)
+    println(io, "and edge data:")
+    show(io, mime, edge_data(graph))
+    return nothing
+  end
+
+  show(io::IO, graph::AbstractDataGraph) = show(io, MIME"text/plain"(), graph)
 
   #
   # DataGraph
@@ -38,33 +173,82 @@
     parent_graph::G
     vertex_data::Dictionary{V,VD}
     edge_data::Dictionary{E,ED}
-    function DataGraph{VD,ED}(graph::G) where {VD,ED,G<:AbstractGraph}
-      V = eltype(graph)
-      E = edgetype(graph)
-      vertex_data = similar(Indices(vertices(graph)), Vector{VD})
-      edge_data = similar(Indices(edges(graph)), Vector{ED})
-      display(vertex_data)
-      display(edge_data)
-      return new{VD,ED,V,E,G}(graph, vertex_data, edge_data)
-    end
   end
 
-  #function AbstractDataGraph(
-  #  parent_graph::AbstractGraph,
-  #  vertex_data::AbstractDictionary,
-  #  edge_data::AbstractDictionary)
-  #  V = eltype(parent_graph)
-  #  VD = eltype(vertex_data)
-  #  E = edgetype(parent_graph)
-  #  ED = eltype(edge_data)
-  #  G = typeof(parent_graph)
-  #  return new{V,VD,E,ED,G}(parent_graph, vertex_data, edge_metadata)
-  #end
+  function DataGraph{VD,ED}(
+    parent_graph::G,
+    vertex_data,
+    edge_data
+  ) where {VD,ED,G<:AbstractGraph}
+    V = eltype(parent_graph)
+    E = edgetype(parent_graph)
+    vertex_data = default_data(V, VD, vertices, parent_graph, vertex_data)
+    edge_data = default_data(E, ED, edges, parent_graph, edge_data)
+    return DataGraph{VD,ED,V,E,G}(parent_graph, vertex_data, edge_data)
+  end
 
-  #function DataGraph(graph::AbstractGraph; VertexMetaType=Any, EdgeMetaType=Any)
-  #  # Make a MetaGraph
-  #  vertex_indices = Indices(vertices(graph))
-  #  edge_indices = Indices(edges(graph))
-  #end
+  function DataGraph{VD}(
+    parent_graph::AbstractGraph,
+    vertex_data,
+    edge_data
+  ) where {VD}
+    ED = data_type(edge_data)
+    return DataGraph{VD,ED}(parent_graph, vertex_data, edge_data)
+  end
 
-#end # module DataGraphs
+  function (DataGraph{VD,ED} where {VD})(
+    parent_graph::AbstractGraph,
+    vertex_data,
+    edge_data
+  ) where {ED}
+    VD = data_type(vertex_data)
+    return DataGraph{VD,ED}(parent_graph, vertex_data, edge_data)
+  end
+
+  function DataGraph(
+    parent_graph::AbstractGraph,
+    vertex_data,
+    edge_data
+  )
+    VD = data_type(vertex_data)
+    ED = data_type(edge_data)
+    return DataGraph{VD,ED}(parent_graph, vertex_data, edge_data)
+  end
+
+  #
+  # kwarg versions call arg versions
+  #
+
+  function DataGraph{VD,ED}(
+    parent_graph::AbstractGraph;
+    vertex_data=nothing,
+    edge_data=nothing
+  ) where {VD,ED}
+    return DataGraph{VD,ED}(parent_graph, vertex_data, edge_data)
+  end
+
+  function DataGraph{VD}(
+    parent_graph::AbstractGraph;
+    vertex_data=nothing,
+    edge_data=nothing
+  ) where {VD}
+    return DataGraph{VD}(parent_graph, vertex_data, edge_data)
+  end
+
+  function (DataGraph{VD,ED} where {VD})(
+    parent_graph::AbstractGraph;
+    vertex_data=nothing,
+    edge_data=nothing
+  ) where {ED}
+    return (DataGraph{VD,ED} where {VD})(parent_graph, vertex_data, edge_data)
+  end
+
+  function DataGraph(
+    parent_graph::AbstractGraph;
+    vertex_data=nothing,
+    edge_data=nothing
+  )
+    return DataGraph(parent_graph, vertex_data, edge_data)
+  end
+
+end # module DataGraphs
