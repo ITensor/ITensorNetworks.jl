c9313ed18938c890d93bbf8e4f38e6fafb3f9811
Joey <joeytindall94@googlemail.com>
2022-11-16 13:59:42 -0500

    Removed Redundant Function

--- src/utils.jl
+++ src/utils.jl
@@ -11,16 +11,3 @@ maybe_real(x::Complex) = iszero(imag(x)) ? real(x) : x
 
 maybe_only(x) = x
 maybe_only(x::Tuple{T}) where {T} = only(x)
-
-#Find the edge in the list of edges which involves v1 and v2
-function find_edge(edges::Vector{NamedDimEdge{Tuple}}, v1::Tuple, v2::Tuple)
-  for i in 1:length(edges)
-    if (
-      (src(edges[i]) == v1 && dst(edges[i]) == v2) ||
-      (dst(edges[i]) == v1 && src(edges[i]) == v2)
-    )
-      return i
-    end
-  end
-  return 0
-end
