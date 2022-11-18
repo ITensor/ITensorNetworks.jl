1d5ce1c6953c3fdc417c921e8fd06217b83bdb5c
Joseph Tindall <joeytindall94@googlemail.com>
2022-11-18 14:31:30 -0500

    Introduced function to derive graph from itn

--- src/renameitensornetwork.jl
+++ src/renameitensornetwork.jl
@@ -18,5 +18,16 @@ end
 
 function rename_vertices_itn(psi::ITensorNetwork, name_map::Function)
   original_vertices = vertices(psi)
-  return rename_vertices(psi, Dictionary(original_vertices, name_map.(original_vertices)))
+  return rename_vertices_itn(psi, Dictionary(original_vertices, name_map.(original_vertices)))
+end
+
+#Given an ITN return the underlying graph as a NamedDimGraph
+function getgraph_from_itn(psi::ITensorNetwork)
+  vs = vertices(psi)
+  es = edges(psi)
+  g = NamedDimGraph(vs)
+  for e in es
+    add_edge!(g, src(e), dst(e))
+  end
+  return g
 end
