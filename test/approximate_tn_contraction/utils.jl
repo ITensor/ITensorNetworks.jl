34421771694328e75240c2fb818bccb065c01ee8
LinjianMa <malinjianev@gmail.com>
2022-11-26 16:25:49 -0600

    [ApproxContraction] get contract tests working

--- test/approximate_tn_contraction/utils.jl
+++ test/approximate_tn_contraction/utils.jl
@@ -1,34 +1,39 @@
-using ITensorNetworkAD.ITensorNetworks:
-  ITensor_networks, line_network, TreeTensor, approximate_contract
+using TimerOutputs
+using ITensorNetworks.ApproximateTNContraction: timer, line_network, approximate_contract
 
-@profile function peps_contraction_mpomps(tn; cutoff=1e-15, maxdim=1000, snake=false)
-  N = size(tn)
-  x = tn[:, 1]
-  for i in 2:(N[2] - 1)
-    A = (iseven(i) && snake) ? reverse(tn[:, i]) : tn[:, i]
-    x = contract(MPO(A), MPS(x); cutoff=cutoff, maxdim=maxdim)[:]
+function peps_contraction_mpomps(tn; cutoff=1e-15, maxdim=1000, snake=false)
+  @timeit timer "peps_contraction_mpomps" begin
+    N = size(tn)
+    x = tn[:, 1]
+    for i in 2:(N[2] - 1)
+      A = (iseven(i) && snake) ? reverse(tn[:, i]) : tn[:, i]
+      x = contract(MPO(A), MPS(x); cutoff=cutoff, maxdim=maxdim)[:]
+    end
+    return contract(x..., tn[:, N[2]]...)
   end
-  return contract(x..., tn[:, N[2]]...)
 end
 
-@profile function contract_w_sweep(tn, rank)
-  sweep = sweep_contract(tn, rank, rank)
-  return ldexp(sweep...)
-end
-
-@profile function contract_element_group(tnet, rank)
-  element_grouping = line_network(tnet)
-  return approximate_contract(
-    element_grouping; cutoff=1e-15, maxdim=rank, maxsize=1e15, algorithm="mincut"
-  )
-end
-
-@profile function contract_line_group(tnet, rank, N)
-  line_grouping = SubNetwork(tnet[:, 1])
-  for i in 2:N[2]
-    line_grouping = SubNetwork(line_grouping, tnet[:, i]...)
+function contract_w_sweep(tn, rank)
+  @timeit timer "contract_w_sweep" begin
+    sweep = sweep_contract(tn, rank, rank)
+    return ldexp(sweep...)
   end
-  return batch_tensor_contraction(
-    TreeTensor, [line_grouping]; cutoff=1e-15, maxdim=rank, optimize=false
-  )
+end
+
+function contract_element_group(tnet; cutoff, maxdim)
+  @timeit timer "contract_element_group" begin
+    element_grouping = line_network(tnet)
+    return approximate_contract(element_grouping; cutoff=cutoff, maxdim=maxdim)
+  end
+end
+
+function contract_line_group(tnet; cutoff, maxdim)
+  N = size(tnet)
+  @timeit timer "contract_line_group" begin
+    tntree = tnet[:, 1]
+    for i in 2:N[2]
+      tntree = [tntree, tnet[:, i]]
+    end
+    return approximate_contract(tntree; cutoff=cutoff, maxdim=maxdim)
+  end
 end
