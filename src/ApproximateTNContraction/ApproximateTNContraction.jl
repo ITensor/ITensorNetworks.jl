9e33bb56eab444d7ba17d91ccb7f7dd3227d1ad9
LinjianMa <malinjianev@gmail.com>
2023-01-06 22:30:40 -0600

    [ApproxContraction] Retire unused itensor functions

--- src/ApproximateTNContraction/ApproximateTNContraction.jl
+++ src/ApproximateTNContraction/ApproximateTNContraction.jl
@@ -9,7 +9,6 @@ using TimerOutputs
 
 const timer = TimerOutput()
 
-include("ITensors.jl")
 include("orthogonal_tensor.jl")
 include("networks/itensor_network.jl")
 include("interfaces/sweep_contractor.jl")
