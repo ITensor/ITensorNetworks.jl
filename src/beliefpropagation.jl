aa7b9e10e09217110b9d28183513a0660c635e3a
Joseph Tindall <joeytindall94@googlemail.com>
2023-01-11 10:29:42 -0500

    Simplified BP Further

--- src/beliefpropagation.jl
+++ src/beliefpropagation.jl
@@ -49,11 +49,8 @@ function update_mt(
   contraction_sequence::Function=tn -> contraction_sequence(tn; alg="optimal"),
 )
   contract_list = [mts; [tn[v] for v in subgraph_vertices]]
-  new_mt = if isone(length(contract_list))
-    copy(only(contract_list))
-  else
-    contract(contract_list; sequence=contraction_sequence(contract_list))
-  end
+  
+  new_mt = isone(length(contract_list)) ? copy(only(contract_list)) : contract(contract_list; sequence=contraction_sequence(contract_list))
   return normalize!(new_mt)
 end
 
@@ -73,12 +70,7 @@ function update_all_mts(
 )
   update_mts = copy(mts)
   for e in edges(mts)
-    environment_tensors = ITensor[]
-    for neighboring_subgraph in neighbors(mts, src(e))
-      if (neighboring_subgraph != dst(e))
-        push!(environment_tensors, mts[neighboring_subgraph => src(e)])
-      end
-    end
+    environment_tensors = ITensor[mts[e_in] for e_in in setdiff(boundary_edges(mts, src(e), dir=:in), [reverse(e)])]
     update_mts[src(e) => dst(e)] = update_mt(
       tn, mts[src(e)], environment_tensors; contraction_sequence
     )
@@ -98,42 +90,43 @@ function update_all_mts(
   return mts
 end
 
-#Given a subet of vertices of a given Tensor Network and the Message Tensors for that network, return a Dictionary with the involved subgraphs as keys and the vector of tensors associated with that subgraph as values
-#Specifically, the contraction of the environment tensors and tn[vertices] will be a scalar.
+"""
+Given a subet of vertices of a given Tensor Network and the Message Tensors for that network, return a Dictionary with the involved subgraphs as keys and the vector of tensors associated with that subgraph as values
+Specifically, the contraction of the environment tensors and tn[vertices] will be a scalar.
+"""
 function get_environment(tn::ITensorNetwork, mts::DataGraph, verts::Vector)
   subgraphs = unique([find_subgraph(v, mts) for v in verts])
-  #Need to get these 'boundary edges' in the right direction, probably should raise as an issue to be done on NamedGraphs end
-  env_tensors = ITensor[mts[v] for v in [dst(e) ∈ subgraphs ? e : reverse(e)  for e in boundary_edges(mts, subgraphs)]]
-  
+  env_tensors =  ITensor[mts[e] for e in boundary_edges(mts, subgraphs; dir = :in)]
   return vcat(env_tensors, [tn[v] for v in setdiff(flatten([vertices(mts[s]) for s in subgraphs]), verts)])
 end
 
-#Function to calculate the contraction of a tensor network centred on the vertices verts. Using message tensors.
-function calculate_contraction(tn::ITensorNetwork, mts::DataGraph, verts::Vector, verts_tensors::Vector{ITensor})
+"""
+Calculate the contraction of a tensor network centred on the vertices verts. Using message tensors.
+Defaults to using tn[verts] as the local network but can be overriden
+"""
+function calculate_contraction(tn::ITensorNetwork, mts::DataGraph, verts::Vector; verts_tensors = ITensor[tn[v] for v in verts], contraction_sequence::Function=tn -> contraction_sequence(tn; alg="optimal"))
   environment_tensors = get_environment(tn, mts, verts)
-  return contract(vcat(environment_tensors, verts_tensors); sequence = contraction_sequence(vcat(environment_tensors, verts_tensors)))
+  tensors_to_contract = vcat(environment_tensors, verts_tensors)
+  return contract(tensors_to_contract; sequence = contraction_sequence(tensors_to_contract))
 end
 
-#Function to initialise and update message tensors of a tensornetwork
+"""
+Simulaneously initialise and update message tensors of a tensornetwork
+"""
 function compute_message_tensors(tn::ITensorNetwork; niters = 10, nvertices_per_partition = 1, vertex_groups = nothing, kwargs...)
-  if(vertex_groups == nothing)
-    Z = partition(tn; nvertices_per_partition = nvertices_per_partition)
-  else
-    Z = partition(tn, vertex_groups)
-  end
+
+  Z = vertex_groups == nothing ? partition(tn; nvertices_per_partition = nvertices_per_partition) : partition(tn, vertex_groups)
 
   mts = construct_initial_mts(tn, Z; kwargs...)
   mts = update_all_mts(tn, mts, niters)
   return mts
 end
 
-#Check env is the correct environment for the subset of vertices of tn
+"""
+Check env is the correct environment for the subset of vertices of tn
+"""
 function assert_correct_environment(tn::ITensorNetwork, env::Vector{ITensor}, verts::Vector)
 
-  env_indices = uniqueinds(flatten([inds(e) for e in env]))
-  #Find indices which don't have a duplicate, messy but commoninds(Vector{ITensor} doesn't work)
-  outer_env_indices= env_indices[filter(n -> n!= 0, [count(i -> (i==env_indices[j]), env_indices) == 1 ? j : 0 for j = 1:length(env_indices)])]
   outer_verts_indices = flatten([commoninds(tn, e) for e in boundary_edges(tn, verts)])
-
-  return issetequal(outer_env_indices, outer_verts_indices)
+  return issetequal(noncommoninds(env...), outer_verts_indices)
 end
