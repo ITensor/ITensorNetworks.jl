febc58f6d9f4038cadd86486f4d6506fb7b495ce
Joseph Tindall <51231103+JoeyT1994@users.noreply.github.com>
2023-02-28 08:30:35 -0500

    Symmetric gauge on general graphs

---
+++ src/gauging.jl
@@ -0,0 +1,53 @@
+"""Put an ITensorNetwork into the symmetric gauge and also return the message tensors (which are the diagonal bond matrices)"""
+function symmetric_gauge(
+  ψ::ITensorNetwork;
+  eigen_message_tensor_cutoff=10 * eps(real(scalartype(ψ))),
+  regularization=10 * eps(real(scalartype(ψ))),
+  niters=30,
+)
+  ψψ = ψ ⊗ prime(dag(ψ); sites=[])
+  vertex_groups = nested_graph_leaf_vertices(partition(ψψ, group(v -> v[1], vertices(ψψ))))
+  mts = compute_message_tensors(ψψ; vertex_groups, niters)
+
+  ψsymm = copy(ψ)
+  symm_mts = copy(mts)
+
+  for e in edges(ψ)
+    vsrc, vdst = src(e), dst(e)
+
+    s1, s2 = find_subgraph((vsrc, 1), mts), find_subgraph((vdst, 1), mts)
+    edge_ind = commoninds(mts[s1 => s2], ψsymm[vsrc])
+    edge_ind_sim = sim(edge_ind)
+
+    X_D, X_U = eigen(mts[s1 => s2]; ishermitian=true, cutoff=eigen_message_tensor_cutoff)
+    Y_D, Y_U = eigen(mts[s2 => s1]; ishermitian=true, cutoff=eigen_message_tensor_cutoff)
+    X_D, Y_D = map_diag(x -> x + regularization, X_D),
+    map_diag(x -> x + regularization, Y_D)
+
+    rootX_D, rootY_D = sqrt_diag(X_D), sqrt_diag(Y_D)
+    inv_rootX_D, inv_rootY_D = invsqrt_diag(X_D), invsqrt_diag(Y_D)
+    rootX = X_U * rootX_D * prime(dag(X_U))
+    rootY = Y_U * rootY_D * prime(dag(Y_U))
+    inv_rootX = X_U * inv_rootX_D * prime(dag(X_U))
+    inv_rootY = Y_U * inv_rootY_D * prime(dag(Y_U))
+
+    ψsymm[vsrc] = noprime(ψsymm[vsrc] * inv_rootX)
+    ψsymm[vdst] = noprime(ψsymm[vdst] * inv_rootY)
+
+    Ce = rootX * replaceinds(rootY, edge_ind, edge_ind_sim)
+
+    U, S, V = svd(Ce, edge_ind)
+    rootS = sqrt_diag(S)
+
+    ψsymm[vsrc] = replaceinds(ψsymm[vsrc] * U * rootS, commoninds(S, V), edge_ind)
+    ψsymm[vdst] = replaceinds(ψsymm[vdst], edge_ind, edge_ind_sim)
+    ψsymm[vdst] = replaceinds(ψsymm[vdst] * rootS * V, commoninds(U, S), edge_ind)
+
+    S = replaceinds(
+      S, [commoninds(S, U)..., commoninds(S, V)...] => [edge_ind..., prime(edge_ind)...]
+    )
+    symm_mts[s1 => s2], symm_mts[s2 => s1] = S, S
+  end
+
+  return ψsymm, symm_mts
+end
---
+++ test/test_symmetriseitensornetwork.jl
@@ -0,0 +1,37 @@
+using ITensors
+using ITensorNetworks
+using ITensorNetworks:
+  compute_message_tensors, nested_graph_leaf_vertices, contract_inner, symmetric_gauge
+using NamedGraphs
+using Test
+using Compat
+using Random
+using SplitApplyCombine
+
+@testset "symmetrise_itensornetwork" begin
+  n = 3
+  dims = (n, n)
+  g = named_grid(dims)
+  s = siteinds("S=1/2", g)
+  χ = 3
+
+  Random.seed!(5467)
+  ψ = randomITensorNetwork(s; link_space=χ)
+  ψ_symm, ψ_symm_mts = symmetric_gauge(ψ; niters=50)
+
+  #Test we just did a gauge transform and didn't change the overall network
+  @test contract_inner(ψ_symm, ψ) /
+        sqrt(contract_inner(ψ_symm, ψ_symm) * contract_inner(ψ, ψ)) ≈ 1.0
+
+  ψψ_symm = ψ_symm ⊗ prime(dag(ψ_symm); sites=[])
+  vertex_groups = nested_graph_leaf_vertices(
+    partition(ψψ_symm, group(v -> v[1], vertices(ψψ_symm)))
+  )
+  ψ_symm_mts_V2 = compute_message_tensors(ψψ_symm; vertex_groups=vertex_groups, niters=50)
+
+  for e in edges(ψ_symm_mts_V2)
+    #Test all message tensors are approximately diagonal
+    m_e = ψ_symm_mts_V2[e]
+    @test diagITensor(vector(diag(m_e)), inds(m_e)) ≈ m_e atol = 1e-8
+  end
+end
