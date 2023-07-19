7e9ce10ecb053c86cebf7e9a3110f3047eb8b809
LinjianMa <malinjianev@gmail.com>
2023-07-18 21:06:00 -0500

    change runtest back

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using ITensorNetworks
 @testset "ITensorNetworks.jl, test directory $root" for (root, dirs, files) in walkdir(
   joinpath(pkgdir(ITensorNetworks), "test")
 )
-  test_files = ["test_binary_tree_partition.jl"]# filter!(f -> occursin(Glob.FilenameMatch("test_*.jl"), f), files)
+  test_files = filter!(f -> occursin(Glob.FilenameMatch("test_*.jl"), f), files)
   @testset "Test file $test_file" for test_file in test_files
     println("Running test file $test_file")
     @time include(joinpath(root, test_file))
