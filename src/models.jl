c03b359080e4ad4b78dc97c7b41039d1b4091a40
Benedikt Kloss <bene.kloss@gmail.com>
2024-01-21 11:40:13 -0500

    Implement suggested improvements to model, including general nth-neighbor function.

--- src/models.jl
+++ src/models.jl
@@ -1,24 +1,31 @@
 _maybe_fill(x, n) = x
 _maybe_fill(x::Number, n) = fill(x, n)
 
-function tight_binding(g::AbstractGraph; t=1.0, tp=0.0, h::Union{<:Real,Vector{<:Real}}=0)
+function nth_nearest_neighbors(g,v,n::Int)  #ToDo: Add test for this.
+  if n>=2
+    return setdiff(neighborhood(g, v, n), union(neighborhood(g, v, n-1),neighborhood(g, v, n-2)))
+  else
+    return neighborhood(g, v, 1)
+  end
+end
+
+next_nearest_neighbors(g,v) = nth_nearest_neighbors(g,v,2)
+
+function tight_binding(g::AbstractGraph; t=1, tp=0, h=0)
   h = _maybe_fill(h, nv(g))
   ℋ = OpSum()
   if !iszero(t)
     for e in edges(g)
-      ℋ += -t, "Cdag", maybe_only(src(e)), "C", maybe_only(dst(e))
-      ℋ += -t, "Cdag", maybe_only(dst(e)), "C", maybe_only(src(e))
+      ℋ -= t, "Cdag", maybe_only(src(e)), "C", maybe_only(dst(e))
+      ℋ -= t, "Cdag", maybe_only(dst(e)), "C", maybe_only(src(e))
     end
   end
   if !iszero(t')
     # TODO, more clever way of looping over next to nearest neighbors?
     for (i, v) in enumerate(vertices(g))
-      nnn = [neighbors(g, n) for n in neighbors(g, v)]
-      nnn = setdiff(Base.Iterators.flatten(nnn), neighbors(g, v))
-      nnn = setdiff(nnn, vertices(g)[1:i])
-      for nn in nnn
-        ℋ += -tp, "Cdag", maybe_only(v), "C", maybe_only(nn)
-        ℋ += -tp, "Cdag", maybe_only(nn), "C", maybe_only(v)
+      for nn in next_nearest_neighbors(g,v)
+        ℋ -= tp, "Cdag", maybe_only(v), "C", maybe_only(nn)
+        ℋ -= tp, "Cdag", maybe_only(nn), "C", maybe_only(v)
       end
     end
   end
@@ -31,30 +38,27 @@ function tight_binding(g::AbstractGraph; t=1.0, tp=0.0, h::Union{<:Real,Vector{<
 end
 
 """
-t-t' Hubbard Model 
+t-t' Hubbard Model g,i,v
 """
-function hubbard(g::AbstractGraph; U=0.0, t=1.0, tp=0.0, h::Union{<:Real,Vector{<:Real}}=0)
+function hubbard(g::AbstractGraph; U=0, t=1, tp=0, h=0)
   h = _maybe_fill(h, nv(g))
   ℋ = OpSum()
   if !iszero(t)
     for e in edges(g)
-      ℋ += -t, "Cdagup", maybe_only(src(e)), "Cup", maybe_only(dst(e))
-      ℋ += -t, "Cdagup", maybe_only(dst(e)), "Cup", maybe_only(src(e))
-      ℋ += -t, "Cdagdn", maybe_only(src(e)), "Cdn", maybe_only(dst(e))
-      ℋ += -t, "Cdagdn", maybe_only(dst(e)), "Cdn", maybe_only(src(e))
+      ℋ -= t, "Cdagup", maybe_only(src(e)), "Cup", maybe_only(dst(e))
+      ℋ -= t, "Cdagup", maybe_only(dst(e)), "Cup", maybe_only(src(e))
+      ℋ -= t, "Cdagdn", maybe_only(src(e)), "Cdn", maybe_only(dst(e))
+      ℋ -= t, "Cdagdn", maybe_only(dst(e)), "Cdn", maybe_only(src(e))
     end
   end
   if !iszero(tp)
     # TODO, more clever way of looping over next to nearest neighbors?
     for (i, v) in enumerate(vertices(g))
-      nnn = [neighbors(g, n) for n in neighbors(g, v)]
-      nnn = setdiff(Base.Iterators.flatten(nnn), neighbors(g, v))
-      nnn = setdiff(nnn, vertices(g)[1:i])
-      for nn in nnn
-        ℋ += -tp, "Cdagup", maybe_only(v), "Cup", maybe_only(nn)
-        ℋ += -tp, "Cdagup", maybe_only(nn), "Cup", maybe_only(v)
-        ℋ += -tp, "Cdagdn", maybe_only(v), "Cdn", maybe_only(nn)
-        ℋ += -tp, "Cdagdn", maybe_only(nn), "Cdn", maybe_only(v)
+      for nn in next_nearest_neighbors(g,v)
+        ℋ -= tp, "Cdagup", maybe_only(v), "Cup", maybe_only(nn)
+        ℋ -= tp, "Cdagup", maybe_only(nn), "Cup", maybe_only(v)
+        ℋ -= tp, "Cdagdn", maybe_only(v), "Cdn", maybe_only(nn)
+        ℋ -= tp, "Cdagdn", maybe_only(nn), "Cdn", maybe_only(v)
       end
     end
   end
@@ -72,7 +76,7 @@ end
 """
 Random field J1-J2 Heisenberg model on a general graph
 """
-function heisenberg(g::AbstractGraph; J1=1.0, J2=0.0, h::Union{<:Real,Vector{<:Real}}=0)
+function heisenberg(g::AbstractGraph; J1=1, J2=0, h=0)
   h = _maybe_fill(h, nv(g))
   ℋ = OpSum()
   if !iszero(J1)
@@ -83,12 +87,8 @@ function heisenberg(g::AbstractGraph; J1=1.0, J2=0.0, h::Union{<:Real,Vector{<:R
     end
   end
   if !iszero(J2)
-    # TODO, more clever way of looping over next to nearest neighbors?
     for (i, v) in enumerate(vertices(g))
-      nnn = [neighbors(g, n) for n in neighbors(g, v)]
-      nnn = setdiff(Base.Iterators.flatten(nnn), neighbors(g, v))
-      nnn = setdiff(nnn, vertices(g)[1:i])
-      for nn in nnn
+      for nn in next_nearest_neighbors(g,v)
         ℋ += J2 / 2, "S+", maybe_only(v), "S-", maybe_only(nn)
         ℋ += J2 / 2, "S-", maybe_only(v), "S+", maybe_only(nn)
         ℋ += J2, "Sz", maybe_only(v), "Sz", maybe_only(nn)
@@ -97,7 +97,7 @@ function heisenberg(g::AbstractGraph; J1=1.0, J2=0.0, h::Union{<:Real,Vector{<:R
   end
   for (i, v) in enumerate(vertices(g))
     if !iszero(h[i])
-      ℋ -= h[i], "Sz", maybe_only(v)
+      ℋ += h[i], "Sz", maybe_only(v)
     end
   end
   return ℋ
@@ -106,7 +106,7 @@ end
 """
 Next-to-nearest-neighbor Ising model (ZZX) on a general graph
 """
-function ising(g::AbstractGraph; J1=-1.0, J2=0.0, h::Union{<:Real,Vector{<:Real}}=0)
+function ising(g::AbstractGraph; J1=-1, J2=0, h=0)
   h = _maybe_fill(h, nv(g))
   ℋ = OpSum()
   if !iszero(J1)
@@ -115,12 +115,8 @@ function ising(g::AbstractGraph; J1=-1.0, J2=0.0, h::Union{<:Real,Vector{<:Real}
     end
   end
   if !iszero(J2)
-    # TODO, more clever way of looping over next to nearest neighbors?
     for (i, v) in enumerate(vertices(g))
-      nnn = [neighbors(g, n) for n in neighbors(g, v)]
-      nnn = setdiff(Base.Iterators.flatten(nnn), neighbors(g, v))
-      nnn = setdiff(nnn, vertices(g)[1:i])
-      for nn in nnn
+      for nn in next_nearest_neighbors(g,v)
         ℋ += J2, "Sz", maybe_only(v), "Sz", maybe_only(nn)
       end
     end
