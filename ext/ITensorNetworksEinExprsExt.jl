5b2fe0d0ca810c10adda63bd74fdb49a1ac65974
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-02-06 14:53:35 +0100

    Duplicate code to preserve `tensor_map`

--- ext/ITensorNetworksEinExprsExt.jl
+++ ext/ITensorNetworksEinExprsExt.jl
@@ -29,7 +29,23 @@ function ITensorNetworks.contraction_sequence(
   ::ITensorNetworks.Algorithm"einexpr", tn::Vector{ITensor}; optimizer=EinExprs.Exhaustive()
 )
   tn = ITensorNetwork(tn)
-  path = einexpr(tn; optimizer)
+  IndexType = Any
+
+  tensors = EinExpr{IndexType}[]
+  tensor_map = Dict{Set{IndexType},T}()
+  sizedict = Dict{IndexType,Int}()
+
+  for (key, tensor) in pairs(vertex_data(tn))
+    _inds = collect(ITensorNetworks.inds(tensor))
+    push!(tensors, EinExpr{IndexType}(; head=_inds))
+    tensor_map[Set(_inds)] = key
+    merge!(sizedict, Dict(_inds .=> size(tensor)))
+  end
+
+  _openinds = collect(externalinds(tn))
+  expr = SizedEinExpr(sum(tensors; skip=_openinds), sizedict)
+
+  path = einexpr(optimizer, expr)
 
   function _convert_to_contraction_sequence(subpath)
     EinExprs.nargs(subpath) == 0 && return tensor_map[Set(subpath.head)]
