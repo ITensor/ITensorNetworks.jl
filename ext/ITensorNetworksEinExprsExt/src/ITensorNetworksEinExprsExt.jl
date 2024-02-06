7c92cc1cacce3b093cbe4bc860e460d6af0152e8
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-02-06 16:21:13 +0100

    Apply style suggestion by @mtfishman

--- ext/ITensorNetworksEinExprsExt/src/ITensorNetworksEinExprsExt.jl
+++ ext/ITensorNetworksEinExprsExt/src/ITensorNetworksEinExprsExt.jl
@@ -16,18 +16,18 @@ function prepare_einexpr(tn::ITensorNetwork)
 
   tensor_exprs = EinExpr{IndexType}[]
   tensor_map = Dict{Set{IndexType},VertexType}()
-  sizedict = Dict{IndexType,Int}()
+  inds_dims = Dict{IndexType,Int}()
 
   for v in vertices(tn)
     tensor_v = tn[v]
     inds_v = collect(inds(tensor_v))
     push!(tensor_exprs, EinExpr{IndexType}(; head=inds_v))
     tensor_map[Set(inds_v)] = key
-    merge!(sizedict, Dict(inds_v .=> size(tensor_v)))
+    merge!(inds_dims, Dict(inds_v .=> size(tensor_v)))
   end
 
   externalinds_tn = collect(externalinds(tn))
-  expr = SizedEinExpr(sum(tensor_exprs; skip=externalinds_tn), sizedict)
+  expr = SizedEinExpr(sum(tensor_exprs; skip=externalinds_tn), inds_dims)
 
   return expr, tensor_map
 end
