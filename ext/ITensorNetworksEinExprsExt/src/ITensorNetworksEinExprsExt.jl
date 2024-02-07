c2d9f9d59d1784308219ac691fac1e3196ffe713
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-02-07 21:49:25 +0100

    Apply suggestions by @JoeyT1994

--- ext/ITensorNetworksEinExprsExt/src/ITensorNetworksEinExprsExt.jl
+++ ext/ITensorNetworksEinExprsExt/src/ITensorNetworksEinExprsExt.jl
@@ -1,37 +1,30 @@
 module ITensorNetworksEinExprsExt
 
-using ITensors: Index, ITensor, Algorithm
+using ITensors: Index, ITensor, Algorithm, noncommoninds
 using ITensorNetworks:
-  ITensorNetworks,
-  ITensorNetwork,
-  vertextype,
-  vertex_data,
-  externalinds,
-  contraction_sequence
+  ITensorNetworks, ITensorNetwork, vertextype, vertex_data, contraction_sequence
 using EinExprs: EinExprs, EinExpr, einexpr, SizedEinExpr
 
-function to_einexpr(tn::AbstractITensorNetwork)
+function to_einexpr(ts::Vector{ITensor})
   IndexType = Any
 
   tensor_exprs = EinExpr{IndexType}[]
   inds_dims = Dict{IndexType,Int}()
 
-  for v in vertices(tn)
-    tensor_v = tn[v]
+  for tensor_v in ts
     inds_v = collect(inds(tensor_v))
     push!(tensor_exprs, EinExpr{IndexType}(; head=inds_v))
     merge!(inds_dims, Dict(inds_v .=> size(tensor_v)))
   end
 
-  externalinds_tn = collect(externalinds(tn))
+  externalinds_tn = collect(noncommoninds(ts...))
   return SizedEinExpr(sum(tensor_exprs; skip=externalinds_tn), inds_dims)
 end
 
-function tensor_inds_to_vertex(tn::AbstractITensorNetwork)
+function tensor_inds_to_vertex(ts::Vector{ITensor})
   mapping = Dict{Set{IndexType},VertexType}()
 
-  for v in vertices(tn)
-    tensor_v = tn[v]
+  for (v, tensor_v) in enumerate(ts)
     inds_v = collect(inds(tensor_v))
     mapping[Set(inds_v)] = v
   end
