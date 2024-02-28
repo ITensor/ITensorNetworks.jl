e2f3f2bd514c986ace08632bd1ac951f69f4883c
Benedikt Kloss <bkloss@flatironinstitute.org>
2024-02-28 11:55:10 -0500

    Remove unnecessary `maybe_only`.

--- src/utils.jl
+++ src/utils.jl
@@ -9,9 +9,6 @@ end
 maybe_real(x::Real) = x
 maybe_real(x::Complex) = iszero(imag(x)) ? real(x) : x
 
-maybe_only(x) = x
-maybe_only(x::Tuple{T}) where {T} = only(x)
-
 front(itr, n=1) = Iterators.take(itr, length(itr) - n)
 tail(itr) = Iterators.drop(itr, 1)
 
