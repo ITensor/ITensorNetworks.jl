166645ee5b9ebf57302dbaa276d5a1dc9d90b103
Benedikt Kloss <bkloss@flatironinstitute.org>
2024-02-29 16:50:53 -0500

    Adapt exponentiate solver to new interface

--- src/solvers/exponentiate.jl
+++ src/solvers/exponentiate.jl
@@ -6,7 +6,6 @@ function exponentiate_updater(
   which_sweep,
   sweep_plan,
   which_region_update,
-  region_kwargs,
   updater_kwargs,
 )
   default_updater_kwargs = (;
@@ -18,10 +17,14 @@ function exponentiate_updater(
     issymmetric=true,
     eager=true,
   )
-
+  # extract time_step and substep
+  (;time_step,substep)=updater_kwargs
+  # remove these from updater_kwargs
+  updater_kwargs=Base.structdiff((;time_step,substep),updater_kwargs)
+  # set defaults for unspecified kwargs
   updater_kwargs = merge(default_updater_kwargs, updater_kwargs)  #last collection has precedence
   result, exp_info = exponentiate(
-    projected_operator![], region_kwargs.time_step, init; updater_kwargs...
+    projected_operator![], time_step, init; updater_kwargs...
   )
   return result, (; info=exp_info)
 end
