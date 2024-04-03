c7f5e4bb8e7c05a58a7ef9939fde96f7522ffb4f
Matt Fishman <mtfishman@users.noreply.github.com>
2024-04-03 07:54:14 -0400

    Delete examples (#153)

--- test/test_examples/test_examples.jl
+++ test/test_examples/test_examples.jl
@@ -4,30 +4,9 @@ using Suppressor: @suppress
 using Test: @testset
 
 @testset "Test examples" begin
-  example_files = [
-    "README.jl",
-    "boundary.jl",
-    "distances.jl",
-    "examples.jl",
-    "mincut.jl",
-    "mps.jl",
-    "peps.jl",
-    "steiner_tree.jl",
-    "dynamics/2d_ising_imag_tebd.jl",
-    "treetensornetworks/comb_tree.jl",
-    "treetensornetworks/spanning_tree.jl",
-    "treetensornetworks/ttn_basics.jl",
-    "treetensornetworks/ttn_type.jl",
-  ]
+  example_files = ["README.jl"]
   @testset "Test $example_file" for example_file in example_files
     @suppress include(joinpath(pkgdir(ITensorNetworks), "examples", example_file))
-  end
-  if !Sys.iswindows()
-    example_files = ["contraction_sequence/contraction_sequence.jl"]
-    @testset "Test $example_file (using KaHyPar, so no Windows support)" for example_file in
-                                                                             example_files
-      @suppress include(joinpath(pkgdir(ITensorNetworks), "examples", example_file))
-    end
   end
 end
 end
