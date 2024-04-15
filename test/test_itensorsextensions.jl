1e99c3cb86ff1f60e91b3dfcbf6c7b4a87159ca5
Joey <joeytindall94@googlemail.com>
2024-04-15 13:37:06 -0400

    Better test names in test_itensorsextensions

--- test/test_itensorsextensions.jl
+++ test/test_itensorsextensions.jl
@@ -8,6 +8,7 @@ using ITensors:
   delta,
   inds,
   noprime,
+  op,
   prime,
   randomITensor,
   replaceind,
@@ -19,59 +20,57 @@ using Test: @test, @testset
 
 Random.seed!(1234)
 @testset "ITensorsExtensions" begin
-  @testset "Test map eigvals without QNS (eltype=$elt, dim=$n)" for elt in (Float32, Float64, Complex{Float32}, Complex{Float64}), n in [2, 3, 5, 10]
-    ...
-  end
-    for eltype in (Float64, ComplexF64)
-      for n in [2, 3, 5, 10]
-        i, j = Index(n, "i"), Index(n, "j")
-        linds, rinds = Index[i], Index[j]
-        A = randn(eltype, (n, n))
-        A = A * A'
-        P = ITensor(A, i, j)
-        sqrtP = map_eigvals(sqrt, P, linds, rinds; ishermitian=true)
-        inv_P = dag(map_eigvals(inv, P, linds, rinds; ishermitian=true))
-        inv_sqrtP = dag(map_eigvals(inv ∘ sqrt, P, linds, rinds; ishermitian=true))
+  @testset "Test map eigvals without QNS (eltype=$elt, dim=$n)" for elt in (
+      Float32, Float64, Complex{Float32}, Complex{Float64}
+    ),
+    n in (2, 3, 5, 10)
 
-        sqrtPdag = replaceind(dag(sqrtP), i, i')
-        P2 = replaceind(sqrtP * sqrtPdag, i', j)
-        @test P2 ≈ P atol = 1e-12
+    i, j = Index(n, "i"), Index(n, "j")
+    linds, rinds = Index[i], Index[j]
+    A = randn(elt, (n, n))
+    A = A * A'
+    P = ITensor(A, i, j)
+    sqrtP = map_eigvals(sqrt, P, linds, rinds; ishermitian=true)
+    inv_P = dag(map_eigvals(inv, P, linds, rinds; ishermitian=true))
+    inv_sqrtP = dag(map_eigvals(inv ∘ sqrt, P, linds, rinds; ishermitian=true))
 
-        invP = replaceind(inv_P, i, i')
-        I = invP * P
-        @test I ≈ delta(eltype, inds(I)) atol = 1e-12
+    sqrtPdag = replaceind(dag(sqrtP), i, i')
+    P2 = replaceind(sqrtP * sqrtPdag, i', j)
+    @test P2 ≈ P
 
-        inv_sqrtP = replaceind(inv_sqrtP, i, i')
-        I = inv_sqrtP * sqrtP
-        @test I ≈ delta(eltype, inds(I)) atol = 1e-12
-      end
-    end
+    invP = replaceind(inv_P, i, i')
+    I = invP * P
+    @test I ≈ delta(elt, inds(I))
+
+    inv_sqrtP = replaceind(inv_sqrtP, i, i')
+    I = inv_sqrtP * sqrtP
+    @test I ≈ delta(elt, inds(I))
   end
 
-  @testset "Test map eigvals with QNS" begin
-    for eltype in (Float64, ComplexF64)
-      for n in [2, 3, 5, 10]
-        i, j = Index.(([QN() => n], [QN() => n]))
-        A = randomITensor(eltype, i, j)
-        P = A * prime(dag(A), i)
-        sqrtP = map_eigvals(sqrt, P, i, i'; ishermitian=true)
-        inv_P = dag(map_eigvals(inv, P, i, i'; ishermitian=true))
-        inv_sqrtP = dag(map_eigvals(inv ∘ sqrt, P, i, i'; ishermitian=true))
+  @testset "Test map eigvals with QNS (eltype=$elt, dim=$n)" for elt in (
+      Float32, Float64, Complex{Float32}, Complex{Float64}
+    ),
+    n in (2, 3, 5, 10)
 
-        new_ind = noprime(sim(i'))
-        sqrtPdag = replaceind(dag(sqrtP), i', new_ind)
-        P2 = replaceind(sqrtP * sqrtPdag, new_ind, i)
-        @test P2 ≈ P atol = 1e-12
+    i, j = Index.(([QN() => n], [QN() => n]))
+    A = randomITensor(elt, i, j)
+    P = A * prime(dag(A), i)
+    sqrtP = map_eigvals(sqrt, P, i, i'; ishermitian=true)
+    inv_P = dag(map_eigvals(inv, P, i, i'; ishermitian=true))
+    inv_sqrtP = dag(map_eigvals(inv ∘ sqrt, P, i, i'; ishermitian=true))
 
-        inv_P = replaceind(inv_P, i', new_ind)
-        I = replaceind(inv_P * P, new_ind, i)
-        @test I ≈ op("I", i) atol = 1e-12
+    new_ind = noprime(sim(i'))
+    sqrtPdag = replaceind(dag(sqrtP), i', new_ind)
+    P2 = replaceind(sqrtP * sqrtPdag, new_ind, i)
+    @test P2 ≈ P
 
-        inv_sqrtP = replaceind(inv_sqrtP, i', new_ind)
-        I = replaceind(inv_sqrtP * sqrtP, new_ind, i)
-        @test I ≈ op("I", i) atol = 1e-12
-      end
-    end
+    inv_P = replaceind(inv_P, i', new_ind)
+    I = replaceind(inv_P * P, new_ind, i)
+    @test I ≈ op("I", i)
+
+    inv_sqrtP = replaceind(inv_sqrtP, i', new_ind)
+    I = replaceind(inv_sqrtP * sqrtP, new_ind, i)
+    @test I ≈ op("I", i)
   end
 end
 end
