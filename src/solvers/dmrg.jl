5c15612a90bf480c7caea366b1f2c6c352cf0403
mtfishman <mfishman@flatironinstitute.org>
2024-05-03 15:34:21 -0400

    Output energy from DMRG

--- src/solvers/dmrg.jl
+++ src/solvers/dmrg.jl
@@ -1,14 +1,25 @@
 using ITensors.ITensorMPS: ITensorMPS, dmrg
 using KrylovKit: KrylovKit
 
+struct ComposedObservers{Observers<:Tuple}
+  observers::Observers
+end
+compose_observers(observers...) = ComposedObservers(observers)
+
+function eigval_observer(; kwargs...)
+  @show kwargs
+  error()
+end
+
 """
 Overload of `ITensors.ITensorMPS.dmrg`.
 """
-
 function ITensorMPS.dmrg(
-  operator, init_state; nsweeps, nsites=2, updater=eigsolve_updater, kwargs...
+  operator, init_state; nsweeps, nsites=2, updater=eigsolve_updater, (sweep_observer!)=nothing, kwargs...
 )
-  return alternating_update(operator, init_state; nsweeps, nsites, updater, kwargs...)
+  sweep_observer! = compose_observers(sweep_observer!, eigval_observer)
+  state = alternating_update(operator, init_state; nsweeps, nsites, updater, sweep_observer!, kwargs...)
+  return eigval, state
 end
 
 """
