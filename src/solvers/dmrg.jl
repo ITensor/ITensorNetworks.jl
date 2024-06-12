0a7355e428b92676e945dae83a204d6f7388ac04
Joey <joeytindall94@googlemail.com>
2024-06-12 16:22:39 -0400

    Revert

--- src/solvers/dmrg.jl
+++ src/solvers/dmrg.jl
@@ -24,39 +24,6 @@ function ITensorMPS.dmrg(
   return eigval, state
 end
 
-function ITensorMPS.dmrg(
-  operators::Vector{ITensorNetwork},
-  init_state;
-  nsweeps,
-  nsites=2,
-  updater=bp_eigsolve_updater,
-  inserter=bp_inserter,
-  extracter=bp_extracter,
-  sweep_plan_func=bp_sweep_plan,
-  bp_sweep_kwargs=(;),
-  (region_observer!)=nothing,
-  kwargs...,
-)
-  eigvals_ref = Ref{Any}()
-  region_observer! = compose_observers(
-    region_observer!, ValuesObserver((; eigvals=eigvals_ref))
-  )
-  state = alternating_update(
-    operators,
-    init_state;
-    nsweeps,
-    nsites,
-    updater,
-    region_observer!,
-    inserter,
-    extracter,
-    sweep_plan_func,
-    kwargs...,
-  )
-  eigval = only(eigvals_ref[])
-  return eigval, state
-end
-
 """
 Overload of `KrylovKit.eigsolve`.
 """
