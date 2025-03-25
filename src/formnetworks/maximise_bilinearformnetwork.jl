159966998230301d523f3ade013039a6f2db587f
Joey <joeytindall94@googlemail.com>
2025-03-25 12:42:09 -0400

    Improved solver interface

--- src/formnetworks/maximise_bilinearformnetwork.jl
+++ src/formnetworks/maximise_bilinearformnetwork.jl
@@ -13,13 +13,12 @@ using NDTensors: Algorithm
 using Dictionaries
 using LinearAlgebra: norm_sqr
 
-default_fit_algorithm() = "orthogonalize"
-default_fit_kwargs() = (; niters = 20, nsites = 1, tolerance = 1e-10, normalize = true)
+default_solver_algorithm() = "orthogonalize"
+default_solver_kwargs() = (; niters = 25, nsites = 1, tolerance = 1e-10, normalize = true, maxdim = nothing, cutoff = nothing)
 
-function default_update_sequence(tn::AbstractITensorNetwork; nsites::Int64=1)
-    @assert is_tree(tn)
+function blf_update_sequence(g::AbstractGraph; nsites::Int64=1)
     if nsites == 1 || nsites == 2
-        es = post_order_dfs_edges(tn, first(leaf_vertices(tn)))
+        es = post_order_dfs_edges(g, first(leaf_vertices(g)))
         vs = [[src(e), dst(e)] for e in es]
         regions = nsites == 2 ? vs : [[v] for v in unique(reduce(vcat, vs))]
         return vcat(regions, reverse(reverse.(regions)))
@@ -28,7 +27,7 @@ function default_update_sequence(tn::AbstractITensorNetwork; nsites::Int64=1)
     end
 end
 
-function default_fit_updater(alg::Algorithm"orthogonalize", xAy_bpc::AbstractBeliefPropagationCache, y::AbstractITensorNetwork, prev_region::Vector, region::Vector)
+function blf_updater(alg::Algorithm"orthogonalize", xAy_bpc::AbstractBeliefPropagationCache, y::AbstractITensorNetwork, prev_region::Vector, region::Vector)
     path = gauge_path(y, prev_region, region)
     y = gauge_walk(alg, y, path)
     verts = unique(vcat(src.(path), dst.(path)))
@@ -39,33 +38,38 @@ function default_fit_updater(alg::Algorithm"orthogonalize", xAy_bpc::AbstractBel
     return xAy_bpc, y
 end
 
-function default_fit_extracter(xAy_bpc::AbstractBeliefPropagationCache, region::Vector)
+function blf_extracter(xAy_bpc::AbstractBeliefPropagationCache, region::Vector)
     return environment(xAy_bpc, [(v, "ket") for v in region])
 end
 
-function default_fit_inserter(∂xAy_bpc_∂r::Vector{ITensor}, xAy_bpc::AbstractBeliefPropagationCache, y::AbstractITensorNetwork, region::Vector; normalize = true)
+function blf_inserter(∂xAy_bpc_∂r::Vector{ITensor}, xAy_bpc::AbstractBeliefPropagationCache, y::AbstractITensorNetwork, region::Vector; normalize, maxdim, cutoff)
+    yr = contract(∂xAy_bpc_∂r; sequence = "automatic")
     if length(region) == 1
         v = only(region)
-        yv = contract(∂xAy_bpc_∂r; sequence = "automatic")
         if normalize
-            yv /= sqrt(norm_sqr(yv))
+            yr /= sqrt(norm_sqr(yr))
         end
-        y[v] = yv
-        xAy_bpc = update_factor(xAy_bpc, (v, "ket"), dag(yv))
+        y[v] = yr
+    elseif length(region) == 2
+        v1, v2 = first(region), last(region)
+        linds, cind = uniqueinds(y[v1], y[v2]), commonind(y[v1], y[v2])
+        yv1, yv2 = factorize(yr, linds; ortho = "left", tags=tags(cind), cutoff, maxdim)
+        if normalize
+            yv2 /= sqrt(norm_sqr(yv2))
+        end
+        y[v1], y[v2] = yv1, yv2
     else
-        error("Updates with regions bigger than 1 not supported")
+        error("Updates with regions bigger than 2 not currently supported")
     end
+    vertices = [(v, "ket") for v in region]
+    factors = [y[v] for v in region]
+    xAy_bpc = update_factors(xAy_bpc, Dictionary(vertices, factors))
     return y, xAy_bpc
 end
 
-function default_costfunction(xAy::AbstractBeliefPropagationCache, region)
-    if length(region) == 1
-        pv = only(partitionvertices(xAy, [(only(region), "ket")]))
-        c = region_scalar(xAy, pv)
-        return sqrt(c * conj(c))
-    else
-        error("Cost Functions with regions bigger than 1 not supported")
-    end
+function blf_costfunction(xAy::AbstractBeliefPropagationCache, region)
+    verts =  [(v, "ket") for v in region]
+    return contract([environment(xAy, verts); factors(xAy, verts)]; sequence = "automatic")[]
 end
 
 #Optimize over y to maximize <x|A|y> * <y|dag(A)|x> / <y|y> based on a designated partitioning of the bilinearform
@@ -74,18 +78,19 @@ function maximize_bilinearform(
     xAy::BilinearFormNetwork,
     y::ITensorNetwork = dag(ket_network(xAy)),
     partition_verts = group(v -> first(v), vertices(xAy)); 
-    updater = default_fit_updater,
-    extracter = default_fit_extracter,
-    inserter = default_fit_inserter,
-    costfunction = default_costfunction,
-    sequence = default_update_sequence,
-    normalize::Bool,
-    niters::Int64, 
-    nsites::Int64,
-    tolerance::Float64)
+    updater = blf_updater,
+    extracter = blf_extracter,
+    inserter = blf_inserter,
+    costfunction = blf_costfunction,
+    sequence = blf_update_sequence,
+    normalize::Bool = true,
+    niters::Int64 = 25, 
+    nsites::Int64 = 1,
+    tolerance = nothing,
+    maxdim = nothing,
+    cutoff = nothing)
 
     xAy_bpc = BeliefPropagationCache(xAy, partition_verts)
-    @assert is_tree(partitioned_graph(xAy_bpc))
     seq = sequence(y; nsites)
 
     prev_region = collect(vertices(y))
@@ -94,31 +99,31 @@ function maximize_bilinearform(
         for (j, region) in enumerate(seq)
             xAy_bpc, y = updater(alg, xAy_bpc, y, prev_region, region)
             ∂xAy_bpc_∂r = extracter(xAy_bpc, region)
-            y, xAy_bpc = inserter(∂xAy_bpc_∂r, xAy_bpc, y, region; normalize)
+            y, xAy_bpc = inserter(∂xAy_bpc_∂r, xAy_bpc, y, region; normalize, maxdim, cutoff)
             cs[i, j] = costfunction(xAy_bpc, region)
             prev_region = region
         end
-        if i >= 2 && abs(sum(cs[i, :]) - sum(cs[i-1, :])) / length(seq) <= tolerance
-            return y
+        if i >= 2 && (abs(sum(cs[i, :]) - sum(cs[i-1, :]))) / length(seq) <= tolerance
+            return dag(y)
         end
     end
     
-    return y
+    return dag(y)
 end
 
-function Base.truncate(x::AbstractITensorNetwork; maxdim::Int64, kwargs...)
-    y = random_tensornetwork(scalartype(x), siteinds(x); link_space = maxdim)
+function Base.truncate(x::AbstractITensorNetwork; maxdim_init::Int64, kwargs...)
+    y = ITensorNetwork(v -> inds -> delta(inds), siteinds(x); link_space = maxdim_init)
     xIy = BilinearFormNetwork(x, y)
-    return dag(maximize_bilinearform(xIy, y; kwargs...))
+    return maximize_bilinearform(xIy, y; kwargs...)
 end
 
-function ITensors.apply(A::AbstractITensorNetwork, x::AbstractITensorNetwork; maxdim::Int64, kwargs...)
-    y = random_tensornetwork(scalartype(x), siteinds(x); link_space = maxdim)
+function ITensors.apply(A::AbstractITensorNetwork, x::AbstractITensorNetwork; maxdim_init::Int64, kwargs...)
+    y = ITensorNetwork(v -> inds -> delta(inds), siteinds(x); link_space = maxdim_init)
     xAy = BilinearFormNetwork(A, x, y)
-    return dag(maximize_bilinearform(xAy, y; kwargs...))
+    return maximize_bilinearform(xAy, y; kwargs...)
 end
 
-function maximize_bilinearform(xAy::BilinearFormNetwork, args...; fit_alg = default_fit_algorithm(), fit_kwargs = default_fit_kwargs())
-    return maximize_bilinearform(Algorithm(fit_alg), xAy, args...; fit_kwargs...)
+function maximize_bilinearform(xAy::BilinearFormNetwork, args...; alg = default_solver_algorithm(), solver_kwargs = default_solver_kwargs())
+    return maximize_bilinearform(Algorithm(alg), xAy, args...; solver_kwargs...)
 end
 
