f7f5fe04d8beff45db354618f5c0de9140d1b611
Joey <joeytindall94@googlemail.com>
2025-04-03 12:39:20 -0400

    Comment

--- ext/ITensorNetworksTensorOperationsExt/ITensorNetworksTensorOperationsExt.jl
+++ ext/ITensorNetworksTensorOperationsExt/ITensorNetworksTensorOperationsExt.jl
@@ -7,6 +7,7 @@ using TensorOperations: TensorOperations, optimaltree
 
 function ITensorNetworks.contraction_sequence(::Algorithm"optimal", tn::ITensorList)
   network = collect.(inds.(tn))
+  #Converting dims to Float64 to minimize overflow issues
   inds_to_dims = Dict(i => Float64(dim(i)) for i in unique(reduce(vcat, network)))
   seq, _ = optimaltree(network, inds_to_dims)
   return seq
