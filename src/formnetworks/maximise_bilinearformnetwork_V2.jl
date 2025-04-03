db42eb8d51374684daecd50679c2094f633163d8
Joey <joeytindall94@googlemail.com>
2025-04-03 17:22:57 -0400

    Better naming

--- src/formnetworks/maximise_bilinearformnetwork_V2.jl
+++ src/formnetworks/maximise_bilinearformnetwork_V2.jl
@@ -1,22 +1,20 @@
-@kwdef mutable struct OrthogonalLinearFormProblem{State, LinearFormNetwork}
+@kwdef mutable struct FittingProblem{State, OverlapNetwork}
     state::State
-    linearformnetwork::LinearFormNetwork
+    overlapnetwork::OverlapNetwork
     squared_scalar::Number = 0
 end
 
-squared_scalar(O::OrthogonalLinearFormProblem) = O.squared_scalar
-state(O::OrthogonalLinearFormProblem) = O.state
-linearformnetwork(O::OrthogonalLinearFormProblem) = O.linearformnetwork
+squared_scalar(F::FittingProblem) = F.squared_scalar
+state(F::FittingProblem) = F.state
+overlapnetwork(F::FittingProblem) = F.overlapnetwork
 
-function set(O::OrthogonalLinearFormProblem; state = state(O), linearformnetwork = linearformnetwork(O), squared_scalar = squared_scalar(O))
-    return OrthogonalLinearFormProblem(; state, linearformnetwork, squared_scalar)
+function set(F::FittingProblem; state = state(F), overlapnetwork = overlapnetwork(F), squared_scalar = squared_scalar(F))
+    return FittingProblem(; state, linearformnetwork, squared_scalar)
 end
 
-function updater!(O::OrthogonalLinearFormProblem, local_tensor, region; outputlevel, kws...)
-    O.squared_scalar, local_tensor = linearform_updater
-
-function maximize_linearformnetwork_sq(linearformnetwork, init_state; nsweeps, nsites=1, outputlevel = 0, update_kwargs = (;), inserter_kwargs = (;), kws...)
-    init_prob = OrthogonalLinearFormProblem(; state = copy(init_state), linearformnetwork = linearformnetwork)
+function fit_tensornetwork(tn::AbstractITensorNetwork, init_state::AbstractITensorNetwork, vertex_partitioning)
+    overlap_bpc = BeliefPropagationCache(inner_network(tn, init_state), vertex_partitioning)
+    init_prob = FittingProblem(; state = copy(init_state), overlapnetwork = overlap_bpc)
     common_sweep_kwargs = (; nsites, outputlevel, updater_kwargs, inserter_kwargs)
     kwargs_array = [(; common_sweep_kwargs..., sweep = s) for s in 1:nsweeps]
     sweep_iter = sweep_iterator(init_prob, kwargs_array)
