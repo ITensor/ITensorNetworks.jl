c0ae5d090079b54897f26fbfa8cf44ed683ddc5e
Jack Dunham <jdunham@flatironinstitute.org>
2025-09-25 15:07:06 -0400

    Add `EachRegion` adapter that wraps `RegionIterator`, behaving the same but returning a tuple (region, kwargs) at each step

--- src/solvers/adapters.jl
+++ src/solvers/adapters.jl
@@ -1,36 +1,3 @@
-
-#
-# TupleRegionIterator
-#
-# Adapts outputs to be (region, region_kwargs) tuples
-#
-# More generic design? maybe just assuming RegionIterator
-# or its outputs implement some interface function that
-# generates each tuple?
-#
-
-mutable struct TupleRegionIterator{RegionIter}
-  region_iterator::RegionIter
-end
-
-region_iterator(T::TupleRegionIterator) = T.region_iterator
-
-function Base.iterate(T::TupleRegionIterator, which=1)
-  state = iterate(region_iterator(T), which)
-  isnothing(state) && return nothing
-  (current_region, region_kwargs) = current_region_plan(region_iterator(T))
-  return (current_region, region_kwargs), last(state)
-end
-
-"""
-  region_tuples(R::RegionIterator)
-
-The `region_tuples` adapter converts a RegionIterator into an 
-iterator which outputs a tuple of the form (current_region, current_region_kwargs)
-at each step.
-"""
-region_tuples(R::RegionIterator) = TupleRegionIterator(R)
-
 """
   struct PauseAfterIncrement{S<:AbstractNetworkIterator}
 
@@ -48,3 +15,25 @@ increment!(NC::PauseAfterIncrement) = increment!(NC.parent)
 compute!(NC::PauseAfterIncrement) = NC
 
 PauseAfterIncrement(NC::PauseAfterIncrement) = NC
+
+"""
+  struct EachRegion{RegionIterator} <: AbstractNetworkIterator
+
+Wapper adapter that returns a tuple (region, kwargs) at each step rather than the iterator
+itself.
+"""
+struct EachRegion{R<:RegionIterator} <: AbstractNetworkIterator
+  parent::R
+end
+
+# Essential definitions
+Base.length(ER::EachRegion) = length(ER.parent)
+state(ER::EachRegion) = state(ER.parent)
+increment!(ER::EachRegion) = state(ER.parent)
+
+function compute!(ER::EachRegion)
+  # Do the usual compute! for RegionIterator
+  compute!(ER.parent)
+  # But now lets return something useful
+  return current_region_plan(ER)
+end
