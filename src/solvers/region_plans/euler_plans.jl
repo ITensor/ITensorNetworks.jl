613d5336f692880a892e9163479ef6952556cde0
Jack Dunham <jdunham@flatironinstitute.org>
2025-10-07 16:58:28 -0400

    Fix `euler_sweep` returning kwargs not as `NamedTuple`

--- src/solvers/region_plans/euler_plans.jl
+++ src/solvers/region_plans/euler_plans.jl
@@ -2,6 +2,8 @@ using Graphs: dst, src
 using NamedGraphs.GraphsExtensions: default_root_vertex
 
 function euler_sweep(graph; nsites, root_vertex=default_root_vertex(graph), sweep_kwargs...)
+  sweep_kwargs = (; nsites, root_vertex, sweep_kwargs...)
+
   if nsites == 1
     vertices = euler_tour_vertices(graph, root_vertex)
     sweep = [[v] => sweep_kwargs for v in vertices]
