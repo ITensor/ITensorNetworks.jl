0a9f127a0262ae9eb7ddccf09f8d88640a8dffd7
Jack Dunham <jdunham@flatironinstitute.org>
2025-10-10 10:48:20 -0400

    Update `default_kwargs` tests.

--- test/solvers/test_default_kwargs.jl
+++ test/solvers/test_default_kwargs.jl
@@ -1,23 +1,21 @@
 using Test: @test, @testset
-using ITensorNetworks: AbstractProblem, default_kwargs, current_kwargs, RegionIterator, problem
+using ITensorNetworks: AbstractProblem, default_kwargs, RegionIterator, problem, region_kwargs
 
 module KwargsTestModule
 
 using ITensorNetworks
-using ITensorNetworks: AbstractProblem
+using ITensorNetworks: AbstractProblem, @default_kwargs
 
 export TestProblem, NotOurTestProblem, test_function
 
 struct TestProblem <: AbstractProblem end
 struct NotOurTestProblem <: AbstractProblem end
 
-test_function(; bool=false, int=0) = bool, int
-
-function ITensorNetworks.default_kwargs(::typeof(test_function), ::Type{<:AbstractProblem})
-  return (; int=3)
+@default_kwargs astypes = true function test_function(::AbstractProblem; bool=false, int=3)
+  return bool, int
 end
-function ITensorNetworks.default_kwargs(::typeof(test_function), ::Type{<:TestProblem})
-  return (; bool=true)
+@default_kwargs astypes = true function test_function(::TestProblem; bool=true, int=0)
+  return bool, int
 end
 
 end # KwargsTestModule
@@ -25,19 +23,24 @@ end # KwargsTestModule
 @testset "Default kwargs" begin
   using .KwargsTestModule: TestProblem, NotOurTestProblem, test_function
 
-  our_iter = RegionIterator(TestProblem(), ["region" => (; int=1)], 1)
-  not_our_iter = RegionIterator(NotOurTestProblem(), ["region" => (; int=2)], 1)
+  our_iter = RegionIterator(TestProblem(), ["region" => (; test_function_kwargs=(; int=1))], 1)
+  not_our_iter = RegionIterator(NotOurTestProblem(), ["region" => (; test_function_kwargs=(; int=2))], 1)
+
+  kw = region_kwargs(test_function, our_iter)
+  @test kw == (; int=1)
+  kw_not = region_kwargs(test_function, not_our_iter)
+  @test kw_not == (; int=2)
+
+  @info methods(default_kwargs)
 
   # Test dispatch
-  @test default_kwargs(test_function, our_iter) == (; bool=true)
-  @test default_kwargs(test_function, problem(our_iter)) == (; bool=true)
-  @test default_kwargs(test_function, typeof(problem(our_iter))) == (; bool=true)
+  @test default_kwargs(test_function, problem(our_iter)) == (; bool=true, int=0)
+  @test default_kwargs(test_function, problem(our_iter) |> typeof) == (; bool=true, int=0)
 
-  @test default_kwargs(test_function, not_our_iter) == (; int=3)
-  @test default_kwargs(test_function, problem(not_our_iter)) == (; int=3)
-  @test default_kwargs(test_function, typeof(problem(not_our_iter))) == (; int=3)
+  @test default_kwargs(test_function, problem(not_our_iter)) == (; bool=false, int=3)
+  @test default_kwargs(test_function, problem(not_our_iter) |> typeof) == (; bool=false, int=3)
 
-  @test test_function(; current_kwargs(test_function, our_iter)...) == (true, 0)
-  @test test_function(; current_kwargs(test_function, not_our_iter)...) == (false, 3)
+  @test test_function(problem(our_iter); default_kwargs(test_function, problem(our_iter); kw...)...) == (true, 1)
+  @test test_function(problem(not_our_iter); default_kwargs(test_function, problem(not_our_iter); kw_not...)...) == (false, 2)
 
 end
