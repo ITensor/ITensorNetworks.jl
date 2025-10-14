bd35f0915b3ac438ecc94528621a45a92154e90e
Jack Dunham <jdunham@flatironinstitute.org>
2025-10-14 13:18:02 -0400

    Add `sweep_solve` method for `EachRegion` adapter.
    A callback occuring at each region can be passed using the `do` syntax
    using this method.

--- src/solvers/sweep_solve.jl
+++ src/solvers/sweep_solve.jl
@@ -24,6 +24,23 @@ end
 
 # I suspect that `sweep_callback` is the more commonly used callback, so allow this to
 # be set using the `do` syntax.
-function sweep_solve(sweep_callback, sweep_iterator; kwargs...)
-  return sweep_solve(sweep_iterator; sweep_callback, kwargs...)
+function sweep_solve(
+  sweep_callback, sweep_iterator; region_callback=default_region_callback
+)
+  return sweep_solve(sweep_iterator; sweep_callback, region_callback)
+end
+
+function sweep_solve(
+  each_region_iterator::EachRegion; region_callback=default_region_callback
+)
+  return sweep_solve(region_callback, each_region_iterator)
+end
+function sweep_solve(region_callback, each_region_iterator::EachRegion)
+  for _ in each_region_iterator
+    # I don't think it is obvious what object this particular callback should take,
+    # but for now be consistant and pass the parent sweep iterator.
+    sweep_iterator = each_region_iterator.parent
+    region_callback(sweep_iterator)
+  end
+  return problem(each_region_iterator)
 end
