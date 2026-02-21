620cef7c79d9736f45dfd0fcc7af07c1f35be308
mtfishman <mfishman@flatironinstitute.org>
2026-02-20 21:11:59 -0500

    Apply replace_make_readme patch

--- docs/make_readme.jl
+++ docs/make_readme.jl
@@ -1,21 +1,17 @@
-using ITensorNetworks: ITensorNetworks
 using Literate: Literate
 
-function ccq_logo(content)
-    include_ccq_logo = """
-    <picture>
-      <source media="(prefers-color-scheme: dark)" width="20%" srcset="docs/src/assets/CCQ-dark.png">
-      <img alt="Flatiron Center for Computational Quantum Physics logo." width="20%" src="docs/src/assets/CCQ.png">
-    </picture>
-    """
-    content = replace(content, "{CCQ_LOGO}" => include_ccq_logo)
-    return content
-end
+let inputfile = joinpath(@__DIR__, "..", "examples", "README.jl"),
+        outputdir = joinpath(@__DIR__, ".."), flavor = Literate.CommonMarkFlavor(),
+        name = "README"
 
-Literate.markdown(
-    joinpath(pkgdir(ITensorNetworks), "examples", "README.jl"),
-    joinpath(pkgdir(ITensorNetworks));
-    flavor = Literate.CommonMarkFlavor(),
-    name = "README",
-    postprocess = ccq_logo
-)
+    function postprocess(content)
+        include_ccq_logo = """
+        <picture>
+          <source media="(prefers-color-scheme: dark)" width="20%" srcset="docs/src/assets/CCQ-dark.png">
+          <img alt="Flatiron Center for Computational Quantum Physics logo." width="20%" src="docs/src/assets/CCQ.png">
+        </picture>
+        """
+        return replace(content, "{CCQ_LOGO}" => include_ccq_logo)
+    end
+    Literate.markdown(inputfile, outputdir; flavor, name, postprocess)
+end
